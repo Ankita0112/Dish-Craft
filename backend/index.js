76eaaf3611d82d8a1d0d9455be12c5463fb8bf7e
Ankita <mauryaankita531@gmail.com>
2024-06-27 15:43:16 +0530

    deployment change

--- backend/index.js
+++ backend/index.js
@@ -26,11 +26,11 @@ const app = express();
 // app.use(cors());
 
 
-// app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json())
